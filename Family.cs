c4f28e13f753e94724002441710e4bf218651ec3
LionForder <goomano12@gmail.com>
2022-05-13 19:10:24 +0500

    Family OOP

---
+++ Family.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace OneOOP
+{
+    public class Family
+    {
+        private int groupOfBlood = 1;
+
+        public string WhoFam { get; set; }
+        public string ColorOfHair { get; set; }
+        public string ColorOfEye { get; set; }
+        public string Nationalities { get; set; }
+        public int GroupOfBlood
+        {
+            get { return groupOfBlood; }
+            set
+            {
+                if (value > 0 && value < 5)
+                    groupOfBlood = value;
+                else groupOfBlood = 1;
+            }
+        }
+
+
+        public Family(){}
+        public Family(string who, string hair, string eye, string nationalities, int blood)
+        {
+            WhoFam = who;
+            ColorOfHair = hair;
+            ColorOfEye = eye;
+            Nationalities = nationalities;
+            GroupOfBlood = blood;
+        }
+
+        public static Family operator +(Family dad, Family mom)
+        {
+            return new Family("child", mom.ColorOfHair, dad.ColorOfEye,
+                dad.Nationalities, mom.GroupOfBlood);
+        }
+
+
+        public override string ToString()
+        {
+            return $"{WhoFam} - {Nationalities} with {ColorOfEye} eye," +
+            	$" {ColorOfHair} hair, {GroupOfBlood} group of blood";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace OneOOP
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string eyeD = Console.ReadLine();
+            string hairD = Console.ReadLine();
+            string nationalityD = Console.ReadLine();
+            int bloodD = Convert.ToInt32(Console.ReadLine());
+
+            Family dad = new Family()
+            {
+                WhoFam = "dad",
+                ColorOfEye = eyeD,
+                ColorOfHair = hairD,
+                Nationalities = nationalityD,
+                GroupOfBlood = bloodD
+            };
+
+            string eyeM = Console.ReadLine();
+            string hairM = Console.ReadLine();
+            string nationalityM = Console.ReadLine();
+            int bloodM = Convert.ToInt32(Console.ReadLine());
+
+            Family mom = new Family("mom",hairM, eyeM, nationalityM, bloodM);
+
+            Family child = mom + dad;
+            Console.WriteLine(mom.ToString());
+            Console.WriteLine(dad.ToString());
+            Console.WriteLine(child.ToString());
+        }
+    }
+}
+
+/* поставить switch с вариантами выбора ответа(1, 2, 3..., n)
+ * либо накидать методов с перегрузкой в классе
+ * ласт вариант - ввести свое
+ * 
+ * Сравнивать национальность матери и отца
+ * Если они равны, то национальность ребенка = национальности
+ * родителей, иначе {dad.nationalities}-{mom.nationalities}
+ * 
+ * Когда-нибудь расписать через Random случайность цвета глаз
+ * 
+ * Переписать по группе крови через условия
+ * 
+ * Прописать для WhoFam нормальный вызов
+ */
